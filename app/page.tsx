ae910d76ee9b5892cdb86c0723311ced66fa1511
bingo7894 <pokpong.padjunreed@gmail.com>
2026-03-08 12:47:40 +0700

    update: spilt client and server and function logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Logout Successfully");
+        },
+      },
+    });
+  }
+
   return (
-    <div>
+    <div className="p-24">
       <h1 className="text-2xl font-bold ">Hello World</h1>
 
       <ThemeToggle />
+
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
